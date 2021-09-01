5f599fc8c620bf3aa98e1113110bf4e0e8940ef9
rayudusubrahmanyam12 <rayudusubrahmanyam12@gmail.com>
2021-09-01 17:20:36 +0530

    test

--- vars/fetchRepo.groovy
+++ vars/fetchRepo.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String branchName,String repoURL){
+def call( String branchName , String repoURL ){
     git branch: $branchName, credentialsId: 'GitHub', url: $repoURL
 }
